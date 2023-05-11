eb7d67fb1b79e3f9ab2691c8b5384cd88520b2bc
kaito02050620 <suzuki.engineer06@gmail.com>
2023-05-11 20:40:43 +0900

    完了、未完了の実装が完了しました

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+//todo add
 $("#addTodo").click(function () {
   const inputTodo = $("input").val();
   $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
 
   $("input").val("");
+});
+
+//todo delete
+$(document).on("change", "input[type='checkbox']", function () {
+  if ($(this).is(":checked")) {
+    $(this).parent().css("text-decoration", "line-through");
+  } else {
+    $(this).parent().css("text-decoration", "none");
+  }
 });
